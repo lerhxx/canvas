8ac7e66a787bc058ee56aed714574b326914fee5
lerhxx <407785526@qq.com>
2017-06-13 19:41:37 +0800

    matchGame & toolbar

--- maze/src/js/match.js
+++ maze/src/js/match.js
@@ -1,5 +1,5 @@
 class Mcard extends Card {
-    constructor(obj) {
+    constructor(obj={}) {
         super(obj);
 
         this.deg = 0;
@@ -61,8 +61,9 @@ class Match {
         this.imgMR = 10;         // 图片垂直间距
         this.imgMC = 15;         // 图片水平间距
 
+        this.owner = null;
+
         Object.assign(this, opt);
-        console.log(this.imgW)
 
         if(typeof this.id !== 'string') {
             console.error('There is no id.');
@@ -81,12 +82,12 @@ class Match {
         this.match = [];
         this.matched = 0;
 
-        this.bindEvent = this.bindEvent.bind(this);
         this.addEvent = this.addEvent.bind(this);
         this.removeEvent = this.removeEvent.bind(this);
-        this.changeMaze = this.changeMaze.bind(this);
+        
 
         this.preLoadImg();
+        this.bindEvent();
         this.initMaze();
     }
 
@@ -139,8 +140,7 @@ class Match {
             for(let n = 0; n < this.c; ++n) {
                 this.pathArr[i][n] = new Mcard({
                     r: i,
-                    c: n,
-                    // isMatch: false
+                    c: n
                 });
             }
         }
@@ -186,12 +186,11 @@ class Match {
 
         }
         console.log(this.pathArr);
-        this.bindEvent();
         this.render();
     }
 
     bindEvent() {
-        this.canvas.addEventListener('click', this.addEvent)
+        this.canvas.addEventListener('click', this.addEvent);
     }
 
     addEvent(e) {
@@ -200,25 +199,24 @@ class Match {
             tr = (e.offsetY - this.imgMR - r * (this.imgRH + this.imgMR)) % this.imgRH,     // 鼠标纵坐标实际区域
             tc = (e.offsetX - this.imgMC - c * (this.imgRW + this.imgMC)) % this.imgRW;     // 鼠标横坐标实际区域
             
-            // 鼠标是否位于图片区域
-            if(tr > 10 && tc > 15) {
-                let len = this.match.length,
-                    cur = this.pathArr[r][c];
-                
-                // 已选中两张图片
-                if(len >= 2) {
-                    this.removeEvent();
-                    return;
-                }
-
-                // 已选中0张或1张不同图片
-                if(len === 0 || this.match[0] != cur) {
-                    // cur.isMatch = true;
-                    this.match.push(cur);
-                    cur.rotate();
-                    this.compare();
-                }
+        // 鼠标是否位于图片区域
+        if(tr > 10 && tc > 15) {
+            let len = this.match.length,
+                cur = this.pathArr[r][c];
+            
+            // 已选中两张图片
+            if(len >= 2) {
+                this.removeEvent();
+                return;
             }
+
+            // 已选中0张或1张不同图片
+            if(len === 0 || this.match[0] != cur) {
+                this.match.push(cur);
+                cur.rotate();
+                this.compare();
+            }
+        }
     }
 
     removeEvent() {
@@ -234,20 +232,19 @@ class Match {
 
     matchPic() {
         if(!this.isEqualPic(this.match)) {
-            // this.match[0].isMatch = false;
-            // this.match[1].isMatch = false;
-            
             this.match[0].rotate();
             this.match[1].rotate();
-            // this.render();
         }else {
             this.matched += 2;
+            this.owner.matched();
         }
 
         // 未结束
         if(!this.isEnd()) {
             this.match.length = 0;
             this.bindEvent();
+        }else {
+            this.owner.end();
         }
     }
 
@@ -277,11 +274,151 @@ class Match {
             }
         }
     }
+
+    restart(r=this.r, c=this.c) {
+        this.r = r;
+        this.c = c;
+        this.pathArr = [];
+        this.match.length = 0;
+        this.matched = 0;
+        this.bindEvent();
+        this.initMaze();
+        this.changeMaze();
+    }
 }
 
-new Match({
-    r: 5,
-    c: 4,
+class ToolBar {
+    constructor(obj={}) {
+        this.scoreBox = document.getElementById(obj.score || 'score');
+        this.score = 0;
+    }
+
+    add(score=0) {
+        this.score += score;
+        this.changeScore();
+        return this;
+    }
+
+    substract(score=0) {
+        this.score -= score;
+        this.changeScore();
+        return this;
+    }
+
+    multiply(score = 1) {
+        this.score *= score;
+        this.changeScore();
+        return this;
+    }
+
+    divide(score=1) {
+        if(score === 0) {
+            console.error('The denominator cannot be zero');
+            return;
+        }
+        this.multiply(1 / score);
+        this.changeScore();
+        return this;
+    }
+
+    changeScore(score=this.score) {
+        this.scoreBox.innerHTML = score;
+    }
+
+    clear() {
+        this.score = 0;
+        this.changeScore(0);
+    }
+}
+
+class MatchGame {
+    constructor(obj) {
+        this.r = 4;
+        this.c = 3;
+
+        Object.assign(this, obj);
+
+        obj.owner = this;
+        this.scene = new Match(obj);
+        this.toolBar = new ToolBar();
+
+        // 缓存dom
+        this.rankWrapper = document.getElementById(obj.rankWrapper || 'rank-wrapper');
+        this.rankScore = document.getElementById(obj.rankScore || 'rank-score');
+        this.totalTime =  document.getElementById(obj.totalTime || 'total-time');
+        this.rank = document.getElementById(obj.rank || 'rank');
+
+        this.bindEvent();
+    }
+
+    bindEvent() {
+        let self = this;
+        document.getElementById('start').addEventListener('click', function() {
+            this.parentNode.style.display = 'none';
+            self.startTime = new Date().getTime();
+        })
+        document.getElementById('restart').addEventListener('click', (e) => {
+            self.restart();
+        })
+        document.getElementById('next').addEventListener('click', (e) => {
+            self.next();
+        })
+        document.getElementById('more').addEventListener('click', (e) => {
+            alert('这是个空白按钮哦~哈哈哈');
+        })
+    }
+
+    restart() {
+        this.toolBar.clear();
+        this.scene.restart(this.r, this.c);
+        this.startTime = new Date().getTime();
+        this.rankWrapper.style.display = 'none';
+    }
+
+    next() {
+        this.r += 2;
+        this.c += 1;
+        this.restart();
+    }
+
+    matched() {
+        let now = new Date().getTime();
+        let score = ~~(1000000/(now - this.startTime));
+        this.toolBar.add(score);
+    }
+
+    end() {
+        this.rankWrapper.style.display = 'block';
+        this.rankScore.innerHTML = this.toolBar.score;
+        this.totalTime.innerHTML = this.formateTime(new Date().getTime() - this.startTime);
+        this.rank.classList.add('dump');
+    }
+
+    formateTime(time) {
+        let sec = 0,
+            min = 0,
+            hour = 0,
+            str = '';
+        let HOUR = 60 * 60 * 1000,
+            MIN = 60 * 1000,
+            SEC = 1000;
+
+        hour = ~~(time / HOUR);
+        if(hour >= 24) {
+            return '>1天';
+        }else {
+            time = time % HOUR;
+            min = ~~(time / MIN);
+            time = time % MIN;
+            sec = ~~(time / SEC);
+            return `${hour}：${min}：${sec}`;
+        }
+    }
+}
+
+new MatchGame({
+    r: 2,
+    c: 2,
     id: 'canvas'
 });
 
